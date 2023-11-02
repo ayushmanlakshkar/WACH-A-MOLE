e60684698f522bd7c7616c2c490facbceb364e2e
Ayushman Lakshkar <ayushman.lakshkar7@gmail.com>
2023-11-03 00:36:49 +0530

    timing problem solved

--- Wack-A-Mole/js.js
+++ Wack-A-Mole/js.js
@@ -1,4 +1,3 @@
-let hole = document.getElementsByTagName("li")
 let lost = document.getElementsByClassName("lost")
 let message = document.getElementsByClassName("status")
 let score = document.getElementById("score")
@@ -6,16 +5,15 @@ let start = document.getElementById("start")
 let wrong= new Audio("sounds/wrong.mp3")
 let haar=new Audio("sounds/lost.mp3")
 let show= new Audio("sounds/moleshow.mp3")
+let hole = document.getElementsByClassName("hole")
 wrong.playbackRate=4
 show.playbackRate=2.5
-
+let pappu
 
 check = (data) => {
     if (data.innerHTML != "") {
-        
         score.innerHTML = Number(score.innerHTML) + 100
     }
-
 
     else {
         if (lost[0].innerHTML != 'X') {
@@ -28,42 +26,40 @@ check = (data) => {
             lost[2].innerHTML = 'X'
             haar.play()
             
-            
         }
     } 
+    if (lost[0].innerHTML == 'X'&&lost[1].innerHTML == 'X'&&lost[2].innerHTML == 'X') {
+        clearInterval(pappu)
+        lost[0].innerHTML = ''
+        lost[1].innerHTML = ''
+        lost[2].innerHTML = ''
+           }
 }
 
 
 
 let game = ()=>{
 
-    score.innerHTML = "0"
-    let pappu = setInterval(() => {
+    for (let i = 0; i < hole.length; i++) {
+        hole[i].addEventListener('click', ()=> check (hole[i]))
         
+    }
+
+    score.innerHTML = "0"
+    pappu = setInterval(() => {
         let x = Math.floor(Math.random() * 9)
         hole[x].innerHTML = "<img src='images/whack.jpg'  width= 90px height=90px >"
         show.play()
+
         setInterval(() => {
             hole[x].innerHTML = ""
         }, 1500);
-        if (lost[0].innerHTML == 'X'&&lost[1].innerHTML == 'X'&&lost[2].innerHTML == 'X') {
-            clearInterval(pappu)
-            lost[0].innerHTML = ''
-            lost[1].innerHTML = ''
-               lost[2].innerHTML = ''
-               }
+
     }, 2000);
+
     }
 
     start.onclick= ()=>{game()}
 
 
-hole[0].onclick = ()=>{ check(hole[0])}
-hole[1].onclick = ()=>{ check(hole[1])}
-hole[2].onclick = ()=>{ check(hole[2])}
-hole[3].onclick = ()=>{ check(hole[3])}
-hole[4].onclick = ()=>{ check(hole[4])}
-hole[5].onclick = ()=>{ check(hole[5])}
-hole[6].onclick = ()=>{ check(hole[6])}
-hole[7].onclick = ()=>{ check(hole[7])}
-hole[8].onclick = ()=>{ check(hole[8])}
+
